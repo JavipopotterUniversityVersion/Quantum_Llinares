b162f790db82b4d5b8405109ceb408c3271b364c
Javipopotter <javivals@ucm.es>
2025-03-13 18:09:28 +0100

    Update HealtComponent.cs

--- Assets/Scripts/Enemies/HealtComponent.cs
+++ Assets/Scripts/Enemies/HealtComponent.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HealtComponent : MonoBehaviour
 {
 
     [SerializeField] float _health = 1.0f;
+    [SerializeField] UnityEvent _onDeath;
+    [SerializeField] UnityEvent _onDamage;
    
     public void SetHealth(float h){ _health = h;}
 
@@ -15,8 +18,10 @@ public class HealtComponent : MonoBehaviour
 
     public void GetDamage(float d){
         _health -=d;
+        _onDamage.Invoke();
+        
         if(_health<=0) {
-            Destroy(this.gameObject);
+            _onDeath.Invoke();
         }
     }
     
