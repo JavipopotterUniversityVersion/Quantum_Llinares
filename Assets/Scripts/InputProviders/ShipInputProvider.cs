f3036d61478840a8fc3e2eedbf9fe06efe97e176
Diego <dicaball@ucm.es>
2025-03-13 15:51:20 +0100

    Mutxo input

---
+++ Assets/Scripts/InputProviders/ShipInputProvider.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShipInputProvider : MonoBehaviour
+{
+    [SerializeField] InputActionReference _rotateRight, _rotateLeft, _shootTopCannon, _shootBottomCannon;
+
+    [SerializeField] RotationComponent _shipRotation;
+
+    #region Patron ActionReference
+    private void OnEnable()
+    {
+        _rotateRight.action.Enable();
+        _rotateLeft.action.Enable();
+
+        _shootTopCannon.action.Enable();
+        _shootBottomCannon.action.Enable();
+    }
+
+    private void Awake()
+    {
+        _rotateRight.action.performed += OnRightInputRecieved;
+        _rotateLeft.action.performed += OnLefttInputRecieved;
+
+        //_shootTopCannon.action.performed += OnShootTopInputRecieved;
+        //_shootBottomCannon.action.performed += OnShootBottomInputRecieved;
+    }
+
+    private void OnDisable()
+    {
+        _rotateRight.action.performed -= OnRightInputRecieved;
+        _rotateLeft.action.performed -= OnLefttInputRecieved;
+
+        //_shootTopCannon.action.performed -= OnShootTopInputRecieved;
+        //_shootBottomCannon.action.performed -= OnShootBottomInputRecieved;
+    }
+
+    private void OnDestroy()
+    {
+        _rotateRight.action.Disable();
+        _rotateLeft.action.Disable();
+
+        _shootTopCannon.action.Disable();
+        _shootBottomCannon.action.Disable();
+    }
+    #endregion
+
+    #region actions
+    private void OnRightInputRecieved(InputAction.CallbackContext obj) => _shipRotation.RotateObject(true);
+    private void OnLefttInputRecieved(InputAction.CallbackContext obj) => _shipRotation.RotateObject(false);
+
+    //private void OnShootTopInputRecieved(InputAction.CallbackContext obj) => shootTop
+    //private void OnShootBottomInputRecieved(InputAction.CallbackContext obj) => shootBottom
+    #endregion
+}
