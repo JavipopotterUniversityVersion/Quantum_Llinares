954e09344781cc28846b4abcbcba87a66ad5ff66
davidfb1305 <146523610+David13fb@users.noreply.github.com>
2025-03-14 19:54:39 +0100

    COOLDOWN FACTOR ESQUERE

--- Assets/Scripts/Upgrades/ChangeBulletUpgrade.cs
+++ Assets/Scripts/Upgrades/ChangeBulletUpgrade.cs
@@ -7,10 +7,12 @@ using UnityEngine.Rendering;
 public class ChangeBulletUpgrade : VirtualUpgrade
 {
     [SerializeField]GameObject _bullet;
-    
+    [SerializeField] float _cooldown = 100.0f;
     override public void ApplyUpgrade(GameObject obj) {
         List<ShootComponent> sc = obj.GetComponentsInChildren<ShootComponent>().ToList();
-        sc.OrderBy(canon => Vector2.Distance(canon.transform.position, transform.position)).First().SetBullet(_bullet);
+       ShootComponent scp = sc.OrderBy(canon => Vector2.Distance(canon.transform.position, transform.position)).First();
+        scp.SetBullet(_bullet);
+        scp.SetCooldown(_cooldown);
     }
 
 }
