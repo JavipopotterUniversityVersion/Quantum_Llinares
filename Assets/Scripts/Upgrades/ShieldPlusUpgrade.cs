361b1b21ab8e05a2328ba80db7dd04e18d301db8
fwoggydweller <asanch42@ucm.es>
2025-03-14 21:20:58 +0100

    Upgrades prefab creation, tested
    Prefabs created for the current updates, tested. Rn they have the follow movement (for testing it easily)

--- Assets/Scripts/Upgrades/ShieldPlusUpgrade.cs
+++ Assets/Scripts/Upgrades/ShieldPlusUpgrade.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ShieldPlusUpgrade: VirtualUpgrade
 {
     override public void ApplyUpgrade(GameObject obj)
     {
+        Debug.Log("A");
         ShipHealth h = obj.GetComponent<ShipHealth>();
         int maxShield = h.getMaxShield();
         h.setMaxShield(maxShield+1);
         h.setShieldToMax();
+        Destroy(this.gameObject);
     }
 }
