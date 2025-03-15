bd71e0567b3884f265931e7e7c425b10e3477a84
davidfb1305 <146523610+David13fb@users.noreply.github.com>
2025-03-15 14:28:28 +0100

    menu de muerte

---
+++ Assets/Scripts/Feedback/DeathMenuControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathMenuControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject _background;
+    [SerializeField] GameObject _score;
+    [SerializeField] GameObject _bretry;
+    [SerializeField] GameObject _breturn;
+    public void StartMenuCorrutine()
+    {
+        StartCoroutine(ActiveDeathMenu());
+    }
+
+    public IEnumerator ActiveDeathMenu(){
+        _background.SetActive(true);
+        yield return new WaitForSeconds(1);
+        _score.SetActive(true);
+        yield return new WaitForSeconds(1);
+      _bretry.SetActive(true);
+      _breturn.SetActive(true);
+    }
+}
