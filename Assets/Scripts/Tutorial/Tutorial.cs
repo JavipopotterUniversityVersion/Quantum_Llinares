8e0f926c30ae6436a951e0635998dcb7f7ca7ef6
Diego <dicaball@ucm.es>
2025-03-15 18:17:48 +0100

    Tutoria continua

--- Assets/Scripts/Tutorial/Tutorial.cs
+++ Assets/Scripts/Tutorial/Tutorial.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
-    [SerializeField] String _estoyLoco;
     [SerializeField] StringContainer _container;
+
+    [SerializeField] String[] _dialogs;
+
+    private int _index;
     private void Start()
     {
-        _container.SetValue(_estoyLoco);
+        RunTutorialScene();
+    }
+
+    private void RunTutorialScene()
+    {
+        Time.timeScale = 0.0f;
+        _container.SetValue(_dialogs[_index]);
+    }
+
+    public void UnPause()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    public void advanceInTutorial()
+    {
+        if (_index >= _dialogs.Length) return;
+        _index++;
+        RunTutorialScene();
     }
 }
